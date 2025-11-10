f2fb2569318efa484e3410f66d8ff98b235c8413
PJaros <jaros.paul@gmail.com>
2025-11-10 20:14:43 +0100

    Refactor to store all data in enum.

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use eframe::egui;
 use eframe::egui::{Vec2, Visuals, vec2};
 
 fn main() -> eframe::Result {
-    let app = AppGUI::default();
+    let app = AppGUI::new();
     eframe::run_native(
         "eframe fit content",
         eframe::NativeOptions::default(),
@@ -11,22 +11,20 @@ fn main() -> eframe::Result {
 }
 
 enum Stage {
-    PreRender,
-    FirstRender,
-    FirstResize,
-    Initialized,
+    PreRender(isize),
+    FirstRender(Vec2),
+    FirstResize(Vec2),
+    Initialized(Vec2),
 }
 
 struct AppGUI {
     _render_stage: Stage,
-    _initial_size: Option<Vec2>,
 }
 
-impl Default for AppGUI {
-    fn default() -> Self {
+impl AppGUI {
+    fn new() -> Self {
         Self {
-            _render_stage: Stage::PreRender,
-            _initial_size: None,
+            _render_stage: Stage::PreRender(2_isize),
         }
     }
 }
@@ -47,8 +45,12 @@ impl AppGUI {
         );
         ui.label(format!(
             "self._used_size: {}",
-            self._initial_size
-                .unwrap_or_else(|| vec2(123.012_344, 123.012_344))
+            match self._render_stage {
+                Stage::PreRender(_) => vec2(123.012_344, 123.012_344),
+                Stage::FirstRender(size) => size,
+                Stage::FirstResize(size) => size,
+                Stage::Initialized(size) => size,
+            }
         ));
         for n in 1..=10 {
             ui.label(format!("Line: {}", n));
@@ -60,24 +62,26 @@ impl eframe::App for AppGUI {
     fn update(&mut self, ctx: &eframe::egui::Context, _frame: &mut eframe::Frame) {
         ctx.set_visuals(Visuals::light());
         match self._render_stage {
-            Stage::PreRender => {
+            Stage::PreRender(mut pre_render_cycle) => {
                 self.pre_render(ctx);
-                self._initial_size = Some(ctx.used_size());
-                self._render_stage = Stage::FirstRender;
+                pre_render_cycle -= 1;
+                if pre_render_cycle > 0 {
+                    self._render_stage = Stage::PreRender(pre_render_cycle)
+                } else {
+                    self._render_stage = Stage::FirstRender(ctx.used_size());
+                }
             }
-            Stage::FirstRender => {
+            Stage::FirstRender(size) => {
                 egui::CentralPanel::default().show(ctx, |ui| {
                     self.render(ui);
                 });
-                self._render_stage = Stage::FirstResize;
+                self._render_stage = Stage::FirstResize(size)
             }
-            Stage::FirstResize => {
-                if let Some(size) = self._initial_size {
-                    ctx.send_viewport_cmd(egui::ViewportCommand::InnerSize(size));
-                    self._render_stage = Stage::Initialized;
-                }
+            Stage::FirstResize(size) => {
+                ctx.send_viewport_cmd(egui::ViewportCommand::InnerSize(size));
+                self._render_stage = Stage::Initialized(size)
             }
-            Stage::Initialized => {
+            Stage::Initialized(_size) => {
                 egui::CentralPanel::default().show(ctx, |ui| {
                     self.render(ui);
                 });
